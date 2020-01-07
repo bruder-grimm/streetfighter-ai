afcb17f11c061cb45476e3f56019e38a7edb4e01
BlkPingu <tobias-kolb@hotmail.de>
2020-01-07 19:41:19 +0100

    docker

---
+++ docker/README.md
@@ -0,0 +1,11 @@
+### TOOLCHAIN
+
+
+
+- OpenAI
+  - [Repo]( https://github.com/openai/gym/blob/master/docs/environments.md)
+  - [OpenAI gym](https://gym.openai.com)
+    - gym unterstützt [retro](https://github.com/openai/retro)
+      - retro unterstützt SNES via [Snes9X](https://snes9x.en.uptodown.com/mac)
+        - [SNES ROMs](https://emulator.games/roms/)
+
---
+++ docker/src/proof_of_concet.py
@@ -0,0 +1,9 @@
+import retro
+env = retro.make(game='StreetFighterIISpecialChampionEdition-Genesis')
+
+
+#richtig:   a5aad1d108046d9388e33247610dafb4c6516e0b
+#gefunden:  a5aad1d108046d9388e33247610dafb4c6516e0b
+#bei: https://www.romnation.net/srv/roms/20632/genesis/Street-Fighter-2-Special-Champion-Edition-U.html
+
+#falsch:    2a406e2e4743de98785c85322f858abfb8221ae0
